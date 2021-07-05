8b64f85b441cdad94fdd8220baa40479e9d17460
kimwoojin <wjinkim21@naver.com>
2021-07-05 11:30:11 +0900

    change javascript

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 
 function onLoginSubmit(event){
@@ -20,15 +21,28 @@ function onLoginSubmit(event){
 function onLoginSubmit(event){
  event.preventDefault();
  const username = loginInput.value;
+ localStorage.setItem("USERNAME_KEY", username);
  loginForm.classList.add(HIDDEN_CLASSNAME)
-
- greeting.innerText = `Hello ${username}`;
- greeting.classList.remove(HIDDEN_CLASSNAME)
+paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
 
 
- 
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
+const savedUsername = localStorage.getItem("USERNAME_KEY");
+
+
+if(savedUsername === null){
+   loginForm.classList.remove(HIDDEN_CLASSNAME);
+   loginForm.addEventListener("submit", onLoginSubmit);
+   
+}else{  
+    paintGreetings(savedUsername);
+}
 
 
